7028f2afec830137837ca9dea6ad76b09c94d5ff
Nathan Barrett <30009669+nathan-barrett@users.noreply.github.com>
2022-03-29 10:19:10 -0700

    Add eslintrc and prettierrc, update mozilla-cookie-helper
    Added prettier to help with all of the new eslint warnings, and moved
    the mozilla cookie hlper from bedrock

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,28 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es6": true,
-        "jasmine": true
+    env: {
+        node: true,
+        browser: true,
+        es6: true,
+        jasmine: true
     },
-    "extends": "eslint:recommended",
-    "rules": {
-        "no-global-assign": 2,
-        "indent": [
-            2,
-            4
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "semi": [
-            2,
-            "always"
-        ],
-        "curly": [
-            2,
-            "all"
-        ],
-        "camelcase": [
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {
+        'no-global-assign': 2,
+        'linebreak-style': [2, 'unix'],
+        quotes: [2, 'single'],
+        semi: [2, 'always'],
+        curly: [2, 'all'],
+        camelcase: [
             2,
             {
-                "properties": "always"
+                properties: 'always'
             }
         ],
-        "eqeqeq": [
-            2,
-            "smart"
-        ],
-        "one-var-declaration-per-line": [
-            2,
-            "always"
-        ],
-        "new-cap": 2
+        eqeqeq: [2, 'smart'],
+        'one-var-declaration-per-line': [2, 'always'],
+        'new-cap': 2
     },
-    "globals": {
-        "Mozilla": true
+    globals: {
+        Mozilla: true
     }
 };
